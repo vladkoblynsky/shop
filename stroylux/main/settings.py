de1b944414c25325193fd468b636ff5b3ff5cd64
vladorton <vladkoblinskyi@gmail.com>
2020-11-02 23:04:19 +0300

    Minor fix

--- stroylux/main/settings.py
+++ stroylux/main/settings.py
@@ -72,7 +72,7 @@ EMAIL_URL = os.environ.get("EMAIL_URL")
 SENDGRID_USERNAME = os.environ.get("SENDGRID_USERNAME")
 SENDGRID_PASSWORD = os.environ.get("SENDGRID_PASSWORD")
 if not EMAIL_URL and SENDGRID_USERNAME and SENDGRID_PASSWORD:
-    EMAIL_URL = "smtp://%s:%s@mailbe05.hoster.by:465/?tls=True" % (
+    EMAIL_URL = "smtp://%s:%s@mail.stroyluxdrev.by:587/?tls=True" % (
         SENDGRID_USERNAME,
         SENDGRID_PASSWORD,
     )
@@ -88,7 +88,7 @@ EMAIL_PORT = os.environ.get('EMAIL_PORT') or email_config["EMAIL_PORT"]
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND') or email_config["EMAIL_BACKEND"]
 EMAIL_USE_TLS = email_config["EMAIL_USE_TLS"]
 EMAIL_USE_SSL = email_config["EMAIL_USE_SSL"]
-
+print(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_HOST, EMAIL_PORT, EMAIL_USE_TLS)
 # If enabled, make sure you have set proper storefront address in ALLOWED_CLIENT_HOSTS.
 ENABLE_ACCOUNT_CONFIRMATION_BY_EMAIL = get_bool_from_env(
     "ENABLE_ACCOUNT_CONFIRMATION_BY_EMAIL", False
